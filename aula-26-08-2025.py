79918b3098f9c546896be0a5c65e214dae3a791a
euericsoncosta <costaericson@hotmail.com>
2025-08-27 08:29:31 -0300

    Aula-26/08/2025

--- aula-26-08-2025.py
+++ aula-26-08-2025.py
@@ -1,1 +1,42 @@
-#operadores de 
+#operadores de atribuição
+# =, +=, -=, *=, /=, //=, **=, %=
+
+# numero = 2 
+# print(f" primeiro: {numero}")
+# numero1 = 3 
+
+# numero = numero + 3 
+# print(f" somei 3: {numero}")
+# numero += 3 # mesma coisa que numero = numero + 3
+# print(f" somei 3 de novo: {numero}")
+# numero = numero * 2 
+# print(f" multipliquei por 2: {numero}")
+# numero *= 2 # mesma coisa que numero = numero * 2
+# print(f" multipliquei por 2 de novo: {numero}")
+# numero = numero - 4 
+# print(f" subtrai 4: {numero}")
+# numero -= 4 # mesma coisa que numero = numero - 4
+# print(f" subtrai 4 de novo: {numero}")
+
+# idade = 25
+# tem_carteira = True
+# e_feriado = False
+
+# pode_dirigir = idade >= 18 and tem_carteira
+# print(pode_dirigir)
+# pode_descansar = e_feriado or idade >65
+# print(pode_descansar)
+
+
+
+
+
+idade = int(input("Digite sua idade: "))
+tem_carteira = input("Você tem carteira de motorista? (s/n): ") == "s"
+pode_dirigir = idade >= 18 and tem_carteira
+
+print(f"Pode dirigir: {pode_dirigir}")
+
+
+
+
